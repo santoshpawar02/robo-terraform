c07ac1dd7bae5897af9f83d3e22e2df94c99c884
santoshpawar02 <santosh.pawar02@gmail.com>
2025-03-04 20:27:30 +0530

    route53 records add

--- catalogue.tf
+++ catalogue.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "catalogue" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.small"
   vpc_security_group_ids = ["sg-0acf85a84e4ec3b42"]
   tags = {
-    Name = "frontend"
+    Name = "catalogue"
   }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "catalogue" {
   zone_id = "Z05468031XHYNH8NAZQ8Q"
-  name    = "frontend-prod"
+  name    = "catalogue-prod"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.catalogue.private_ip]
 }
--- mongo.tf
+++ mongo.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "mongo" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.small"
   vpc_security_group_ids = ["sg-0acf85a84e4ec3b42"]
   tags = {
-    Name = "frontend"
+    Name = "mongo"
   }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "mongo" {
   zone_id = "Z05468031XHYNH8NAZQ8Q"
-  name    = "frontend-prod"
+  name    = "mongo-prod"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.mongo.private_ip]
 }
