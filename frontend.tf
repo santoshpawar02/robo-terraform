9a0696b017e5fb968bafa0c7cf1296f7432ec7ec
santoshpawar02 <santosh.pawar02@gmail.com>
2025-03-04 20:12:17 +0530

    sg added

--- frontend.tf
+++ frontend.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.small"
-
+  vpc_security_group_ids = ["sg-0acf85a84e4ec3b42"]
   tags = {
     Name = "frontend"
   }
