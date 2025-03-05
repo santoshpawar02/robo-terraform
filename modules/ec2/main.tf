974edbd3f57f69f210e1bb64ab3e3b23b7fdf4e1
santoshpawar02 <santosh.pawar02@gmail.com>
2025-03-05 17:45:49 +0530

    module

---
+++ modules/ec2/main.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = var.name  
+  }
+}
+
+
+resource "aws_route53_record" "record" {
+  zone_id = var.zone_id
+  name    = "${var.name}-${var.env}"
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.instance[each.key].private_ip]
+}
+
+# resource "null_resource" "catalogue" {
+#   provisioner "remote-exec" {
+
+#     connection {
+#       type     = "ssh"
+#       user     = "ec2-user"
+#       password = "DevOps321"
+#       host     = aws_instance.catalogue.private_ip
+#       }
+
+#     inline = [
+#       "sudo pip3.11 install ansible",
+#       "ansible-pull -i localhost, -U https://github.com/santoshpawar02/robo-ansible roboshop.yml -e component_name=catalogue -e env=prod",
+#     ]
+#   }
+# }
+
+
+# sudo yum install -y yum-utils
+# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+# sudo yum -y install terraform
